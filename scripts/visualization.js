7b7b60937b30caa6701cfc8e406600d11243ce98
Cliffy2000 <cliffyyc@outlook.com>
2025-02-14 14:02:34 -0500

    layout change and width/height update

--- scripts/visualization.js
+++ scripts/visualization.js
@@ -1,8 +1,35 @@
-function generateChart(data, title, width = 700, height = 400, cols = 10, showXAxisTicks = true, showThreshold = true, easeInOut = false, XAxisInverseStatic = false, backgroundEncoding = false, dynamicLabelSize = "none", labelPosition = "follow") {
+/**
+ * Generates a chart with the given data and configuration options.
+ *
+ * @param {Array} data - The data to be visualized in the chart.
+ * @param {string} title - The title of the chart.
+ * @param {number} [width=700] - The overall width of the chart.
+ * @param {number} [height=400] - The overall height of the chart.
+ * @param {number} [pointCount=10] - The number of columns (visible points) in the plot.
+ * @param {boolean} [showXAxisTicks=true] - Whether to show ticks on the X-axis.
+ * @param {boolean} [showThreshold=true] - Whether to show threshold lines.
+ * @param {boolean} [easeInOut=false] - Whether to apply ease-in-out animation.
+ * @param {boolean} [XAxisInverseStatic=false] - Whether the X-axis is inversely static.
+ * @param {boolean} [backgroundEncoding=false] - Whether to use background encoding.
+ * @param {string} [dynamicLabelSize="none"] - The size of the dynamic labels. Possible options: "none", "linear", "ushaped".
+ * @param {string} [labelPosition="follow"] - The position of the labels. Possible options: "follow", "fixed", "side".
+ */
+function generateChart(data, title, width = 700, height = 400, pointCount = 10, showXAxisTicks = true, showThreshold = true, easeInOut = false, XAxisInverseStatic = false, backgroundEncoding = false, dynamicLabelSize = "none", labelPosition = "follow") {
+	// This is the overall size of the entire canvas
+	let TOTAL_WIDTH = width;
+	let TOTAL_HEIGHT = height;
+	// If there is anything to show on the side of the chart
+	const INFO_DEFAULT_WIDTH = 120;
+	
+	let INFO_WIDTH = (labelPosition === "side") ? INFO_DEFAULT_WIDTH : 0;
+	// Plot is the chart image
+	let PLOT_WIDTH = TOTAL_WIDTH - INFO_WIDTH;
+	let PLOT_HEIGHT = TOTAL_HEIGHT;
+
 	const MARGIN = { top: 35, right: 15, bottom: 30, left: 35 };
 	const TEXT_PADDING = { horizontal: 4, vertical: 3 };
 
-	const COLS = cols;
+	const POINTS = pointCount;
 	const OFFSET = 0.5;
 	const X_AXIS_TAIL = 0.6;
 	const AXIS_TICK_SIZE = 4;
@@ -15,22 +42,22 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 	const MIN_THRESHOLD = 30;
 	const MAX_THRESHOLD = 70;
 
-	if (cols > 50) {
+	if (POINTS > 50) {
 		POINT_SIZE = 2.5;
 		AXIS_FONT_SIZE = 7;
-	} else if (cols > 20) {
+	} else if (POINTS > 20) {
 		POINT_SIZE = 3;
 		AXIS_FONT_SIZE = 8;
 	}
 
 	const svg = d3.create("svg")
-		.attr("width", width)
-		.attr("height", height);
+		.attr("width", PLOT_WIDTH)
+		.attr("height", PLOT_HEIGHT);
 
 	const lineChart = svg.append("g");
 
 	const titleText = svg.append("text")
-		.attr("x", width / 2)
+		.attr("x", PLOT_WIDTH / 2)
 		.attr("y", 15)
 		.attr("text-anchor", "middle")
 		.attr("dominant-baseline", "hanging")
@@ -39,16 +66,16 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 		.text(title);
 
 	const x = d3.scaleLinear()
-		.domain([OFFSET, COLS + OFFSET + X_AXIS_TAIL])
-		.range([MARGIN.left, width - MARGIN.right]);
+		.domain([OFFSET, POINTS + OFFSET + X_AXIS_TAIL])
+		.range([MARGIN.left, PLOT_WIDTH - MARGIN.right]);
 
 	const y = d3.scaleLinear()
 		.domain([0, 100])
-		.range([height - MARGIN.bottom, MARGIN.top]);
+		.range([PLOT_HEIGHT - MARGIN.bottom, MARGIN.top]);
 
 	const xAxis = customAxisBottom(x);
 	const xAxisGroup = svg.append("g")
-		.attr("transform", `translate(0,${height - MARGIN.bottom})`)
+		.attr("transform", `translate(0,${PLOT_HEIGHT - MARGIN.bottom})`)
 		.call(xAxis);
 
 	// Remove the ending tick on the axis
@@ -74,8 +101,8 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 	const backgroundRect = svg.insert("rect", ":first-child")
 		.attr("x", MARGIN.left)
 		.attr("y", MARGIN.top)
-		.attr("width", width - MARGIN.left - MARGIN.right)
-		.attr("height", height - MARGIN.top - MARGIN.bottom)
+		.attr("width", PLOT_WIDTH - MARGIN.left - MARGIN.right)
+		.attr("height", PLOT_HEIGHT - MARGIN.top - MARGIN.bottom)
 		.attr("fill", getBackgroundColor(rightPoint.value));
 
 
@@ -86,20 +113,20 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 		.join("line")
 		.attr("class", "horizontal-line")
 		.attr("x1", MARGIN.left)
-		.attr("x2", width - MARGIN.right)
+		.attr("x2", PLOT_WIDTH - MARGIN.right)
 		.attr("y1", d => y(d))
 		.attr("y2", d => y(d))
 		.attr("stroke", "#eee")
 		.style("z-index", -1);
 
 	gridGroup.selectAll(".vertical-line")
-		.data(x.ticks(COLS).slice(0, COLS))
+		.data(x.ticks(POINTS).slice(0, POINTS))
 		.join("line")
 		.attr("class", "vertical-line")
 		.attr("x1", d => x(d))
 		.attr("x2", d => x(d))
 		.attr("y1", MARGIN.top)
-		.attr("y2", height - MARGIN.bottom)
+		.attr("y2", PLOT_HEIGHT - MARGIN.bottom)
 		.attr("stroke", "#eee")
 		.style("z-index", -1);
 
@@ -110,8 +137,8 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 		.append("rect")
 		.attr("x", MARGIN.left + (verticalGap) / 2 - POINT_SIZE)
 		.attr("y", MARGIN.top)
-		.attr("width", verticalGap * (COLS - 1) + POINT_SIZE * 2)
-		.attr("height", height - MARGIN.top - MARGIN.bottom)
+		.attr("width", verticalGap * (POINTS - 1) + POINT_SIZE * 2)
+		.attr("height", PLOT_HEIGHT - MARGIN.top - MARGIN.bottom)
 		.attr("fill", "white");
 
 	const lines = lineChart.append("path")
@@ -181,17 +208,20 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 	function update(step, animTime) {
 		// TODO
 		// Limit ending index
-
 		startIndex = step - 1;
 
-		x.domain([OFFSET + startIndex, COLS + OFFSET + startIndex + X_AXIS_TAIL]);
+		if (startIndex + POINTS > data.length) {
+			return -1;
+		}
+
+		x.domain([OFFSET + startIndex, POINTS + OFFSET + startIndex + X_AXIS_TAIL]);
 
 		const anim = d3.transition("transmove").duration(animTime);
 		if (easeInOut) {
 			anim.ease(d3.easePoly.exponent(3));
 		}
 
-		lastTickValue = startIndex + COLS;
+		lastTickValue = startIndex + POINTS;
 		rightPoint = data[lastTickValue - 1];
 
 		backgroundRect.attr("fill", getBackgroundColor(rightPoint.value));
@@ -222,50 +252,56 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 			.attr("width", textBBox.width + 2 * TEXT_PADDING.horizontal)
 			.attr("height", textBBox.height + 2 * TEXT_PADDING.vertical)
 			.attr("fill", getThresholdColor(rightPoint.value));
+		
+		return 1;
 	}
 
 
 	function resize(newWidth, newHeight) {
-		width = newWidth;
-		height = newHeight;
+		TOTAL_WIDTH = newWidth;
+		TOTAL_HEIGHT = newHeight;
 
-		svg.attr("width", width)
-			.attr("height", height);
+		INFO_WIDTH = (labelPosition === "side") ? INFO_DEFAULT_WIDTH : 0;
+		PLOT_WIDTH = TOTAL_WIDTH - INFO_WIDTH;
+		PLOT_HEIGHT = TOTAL_HEIGHT;
 
-		x.range([MARGIN.left, width - MARGIN.right]);
-		y.range([height - MARGIN.bottom, MARGIN.top]);
+		svg.attr("width", PLOT_WIDTH)
+			.attr("height", PLOT_HEIGHT);
 
-		xAxisGroup.attr("transform", `translate(0,${height - MARGIN.bottom})`)
+		x.range([MARGIN.left, PLOT_WIDTH - MARGIN.right]);
+		y.range([PLOT_HEIGHT - MARGIN.bottom, MARGIN.top]);
+
+		xAxisGroup.attr("transform", `translate(0,${PLOT_HEIGHT - MARGIN.bottom})`)
 			.call(xAxis);
 		yAxisGroup.attr("transform", `translate(${MARGIN.left},0)`)
 			.call(yAxis);
 
-		titleText.attr("x", width / 2);
+		titleText.attr("x", PLOT_WIDTH / 2);
 
 		backgroundRect.attr("x", MARGIN.left)
 			.attr("y", MARGIN.top)
-			.attr("width", width - MARGIN.left - MARGIN.right)
-			.attr("height", height - MARGIN.top - MARGIN.bottom)
+			.attr("width", PLOT_WIDTH - MARGIN.left - MARGIN.right)
+			.attr("height", PLOT_HEIGHT - MARGIN.top - MARGIN.bottom)
 
 		gridGroup.selectAll(".horizontal-line")
 			.data(y.ticks())
 			.join("line")
 			.attr("class", "horizontal-line")
 			.attr("x1", MARGIN.left)
-			.attr("x2", width - MARGIN.right)
+			.attr("x2", PLOT_WIDTH - MARGIN.right)
 			.attr("y1", d => y(d))
 			.attr("y2", d => y(d))
 			.attr("stroke", "#eee")
 			.style("z-index", -1);
 
 		gridGroup.selectAll(".vertical-line")
-			.data(x.ticks(COLS).slice(0, COLS))
+			.data(x.ticks(POINTS).slice(0, POINTS))
 			.join("line")
 			.attr("class", "vertical-line")
 			.attr("x1", d => x(d))
 			.attr("x2", d => x(d))
 			.attr("y1", MARGIN.top)
-			.attr("y2", height - MARGIN.bottom)
+			.attr("y2", PLOT_HEIGHT - MARGIN.bottom)
 			.attr("stroke", "#eee")
 			.style("z-index", -1);
 
@@ -273,8 +309,8 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 
 		clipPath.attr("x", MARGIN.left + (verticalGap) / 2 - POINT_SIZE)
 			.attr("y", MARGIN.top)
-			.attr("width", verticalGap * (COLS - 1) + POINT_SIZE * 2)
-			.attr("height", height - MARGIN.top - MARGIN.bottom);
+			.attr("width", verticalGap * (POINTS - 1) + POINT_SIZE * 2)
+			.attr("height", PLOT_HEIGHT - MARGIN.top - MARGIN.bottom);
 
 		lines.attr("stroke", "#8C8C8C")
 			.attr("stroke-width", 1.5)
@@ -315,12 +351,12 @@ function generateChart(data, title, width = 700, height = 400, cols = 10, showXA
 
 	function customAxisBottom(scale, startX = 1) {
 		const axis = d3.axisBottom(scale)
-			.tickValues(d3.range(startX, startX + COLS))
+			.tickValues(d3.range(startX, startX + POINTS))
 			.tickFormat(showXAxisTicks ? d3.format("d") : "")
 			.tickSizeInner(showXAxisTicks ? AXIS_TICK_SIZE : 0);
 
 		if (XAxisInverseStatic) {
-			axis.tickFormat(showXAxisTicks ? (d, i) => (i + 1 - COLS) : "");
+			axis.tickFormat(showXAxisTicks ? (d, i) => (i + 1 - POINTS) : "");
 		}
 
 		return function (selection) {
